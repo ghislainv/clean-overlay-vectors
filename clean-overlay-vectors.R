d15e3b89578bd50dbb1ab138f9a1821be7bcf776
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2017-10-23 12:13:53 +0200

    Add readme

--- clean-overlay-vectors.R
+++ clean-overlay-vectors.R
@@ -7,44 +7,73 @@
 # license         :GPLv3
 # ==============================================================================
 
-## GRASS GIS 7.2.x is needed to run this script
-## https://grass.osgeo.org/
+# GRASS GIS 7.2.x is needed to run this script
+# https://grass.osgeo.org/
 
-## Libraries
+# Libraries
 library(rgrass7)
 
-##======================================
-## Create new grass location in Lat-Long
+#======================================
+# Download data
+
+dir.create("data")
+d <- "https://nextcloud.fraisedesbois.net/index.php/s/ITnfOi8whKQbOi0/download"
+download.file(url=d,destfile="data/data_ctry_ecozones_FAO.zip",method="wget",quiet=TRUE)
+unzip("data/data_ctry_ecozones_FAO.zip", exdir="data")
+
+# Data includes 
+# 1. Global Administrative Unit Layer from FAO
+# http://ref.data.fao.org/map?entryId=f7e7adb0-88fd-11da-a88f-000d939bc5d8
+# 2. Ecozones from FAO
+# http://ref.data.fao.org/map?entryId=2fb209d0-fd34-4e5e-a3d8-a13c241eb61b&tab=metadata
+
+#======================================
+# Create new grass location in Lat-Long
 
 # dir.create("grassdata")
 # system("grass -c EPSG:4326 -e grassdata/clean-overlay-vectors")  # Ignore errors
 
-## Connect R to grass location
-## Make sure that /usr/lib/grass72/lib is in your PATH in RStudio
+# Connect R to grass location
+# Make sure that /usr/lib/grass72/lib is in your PATH in RStudio
 Sys.setenv(LD_LIBRARY_PATH=paste("/usr/lib/grass72/lib", Sys.getenv("LD_LIBRARY_PATH"),sep=":"))
 initGRASS(gisBase="/usr/lib/grass72",home=tempdir(), 
           gisDbase="grassdata",
           location="deforestmap",mapset="PERMANENT",
           override=TRUE)
 
-# ## Download data
-# d <- "https://nextcloud.fraisedesbois.net/index.php/s/KebuGpEveB161em/download"
-# download.file(url=d,destfile="data/vector_data.zip",method="wget",quiet=TRUE)
-# unzip("data/vector_data.zip", exdir="data")
+#======================================
+# Import and compute overlays
 
-## Import data into GRASS
-## Snapping is necessary
-## See https://en.wikipedia.org/wiki/Decimal_degrees 
-## for correspondance between dd and meters (0.0001 dd ~ 4.3 m)
+# Import ctry country data into GRASS
+# Snapping is necessary
+# See https://en.wikipedia.org/wiki/Decimal_degrees 
+# for correspondance between dd and meters (0.0001 dd ~ 4.3 m)
+system("v.in.ogr --o -o input=data/G2013_2012_0.shp output=ctry snap=0.0001")
+system("g.region n=90 s=-90 w=-180 e=180 res=1 -p")
 
-## Ecozones
+# Create 1 degree size grid:
+system("v.mkgrid map=grid grid=180,360")
+
+# Overlay country boundaries and grid
+system("v.overlay ainput=ctry binput=grid operator=and output=ctry_grid")
+
+# Ecozones
 system("v.in.ogr -o input=data/EcozonesWGS84_4classes.shp output=ecozone snap=1e-7")
 
-## Continents
-system("v.in.ogr -o input=data/Intersect_Gaul2008_Fishnet_AFR.shp output=fishnet_Afr snap=1e-7")
-system("v.in.ogr -o input=data/Intersect_Gaul2008_Fishnet_AFR.shp output=fishnet_SAM snap=1e-7")
-system("v.in.ogr --o -o input=data/Intersect_Gaul2008_Fishnet_Asia.shp output=fishnet_Asia snap=0.0001")
+# Overlay with ecozones
+system("v.overlay ainput=ctry_grid binput=ecozone operator=and output=ctry_grid_ecozone")
 
-## Overlays
-system("v.overlay ainput=fishnet_Afr binput=ecozone operator=and output=overlay_Afr")
-system("v.overlay ainput=fishnet_SAM binput=ecozone operator=and output=overlay_SAM")
+#======================================
+# Export
+
+# Rename columns
+system("v.info -c ctry_grid_ecozone")
+system("v.db.renamecolumn ctry_grid_ecozone column=a_a_ADM0_CODE,ctrycode")
+system("v.db.renamecolumn ctry_grid_ecozone column=a_a_ADM0_NAME,ctryname")
+system("v.db.renamecolumn ctry_grid_ecozone column=b_Reclass,ecozone")
+
+# Export
+dir.create("output")
+system("v.out.ogr input=ctry_grid_ecozone output=output/ctry_grid_ecozone.shp")
+
+# End
